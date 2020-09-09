eff4c3cdbce5a5fdcd3461f3f076bb8665ef42e5
jeppeSS <jsskov92@gmail.com>
2020-09-09 20:48:50 +0200

    Added a few test cases

--- Test/hgl_string_test.c
+++ Test/hgl_string_test.c
@@ -14,7 +14,6 @@ bool
 hgl_sting_new_valid_and_null_terminated( void )
 {
   char *p_hgl_string = hgl_string_new( "abc" );
-  
   if( !p_hgl_string )
   {
     return false;
@@ -24,16 +23,45 @@ hgl_sting_new_valid_and_null_terminated( void )
       p_hgl_string[ 1 ] != 'b' ||
       p_hgl_string[ 2 ] != 'c' )
   {
+    hgl_string_destroy( p_hgl_string );
     return false;
   }
 
   // See if null-terminated.
   if( p_hgl_string[ 3 ] != '\0' )
   {
+    hgl_string_destroy( p_hgl_string );
     return false;
   }
 
+  hgl_string_destroy( p_hgl_string );
   return true;
+}
+
+bool
+hgl_string_length_null_string_size( void )
+{
+  if( hgl_string_length( NULL ) == 0 )
+  {
+    return true;
+  }
+  
+  return false;
+}
+
+bool
+hgl_string_length_valid_string_excluding_null_terminated( void )
+{
+  char *p_hgl_string = hgl_string_new( "abcd" );
+  if( hgl_string_length( p_hgl_string ) == 4 )
+  {
+    hgl_string_destroy( p_hgl_string );
+    return true;
+  }
+  
+  hgl_string_destroy( p_hgl_string );
+  return false;
+
 }
 
 int
@@ -44,10 +72,16 @@ main( void )
        hgl_sting_new_null_input },
 
     { "hgl_sting_new_valid_and_null_terminated",
-       hgl_sting_new_valid_and_null_terminated }
+       hgl_sting_new_valid_and_null_terminated },
+
+    { "hgl_string_length_null_string_size",
+       hgl_string_length_null_string_size },
+
+    { "hgl_string_length_valid_string_excluding_null_terminated",
+       hgl_string_length_valid_string_excluding_null_terminated }
   };
 
-  hgl_unit_run( test_cases, 2 );
+  hgl_unit_run( test_cases, 4 );
 
   return EXIT_SUCCESS;
 }
